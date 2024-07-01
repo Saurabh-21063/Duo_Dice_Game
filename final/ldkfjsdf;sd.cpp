12bd75fd1130a452abceea3a4b6be563117cae52
Saurabh <123747524+Saurabh-21063@users.noreply.github.com>
2024-07-01 19:59:58 +0530

    Add files via upload

---
+++ final/ldkfjsdf;sd.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+int data;
+Node * next;
+
+Node(int data)
+{
+  this->data = data;
+  this->next = NULL;
+}
+};
+int main()
+{
+Node * start = new Node(3);
+
+}
